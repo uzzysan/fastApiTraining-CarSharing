b16f074f4eaa5e984b25fd25cf62407d2f9673fc
Rafał Maculewicz <rafal.maculewicz@hotmail.com>
2025-07-04 15:55:15 +0200

    Uproszczono kod w pliku carsharing.py poprzez zastąpienie statycznej bazy danych funkcją load_db() oraz poprawiono sposób filtrowania samochodów, zmieniając dostęp do atrybutów z notacji słownikowej na notację obiektową. Dodatkowo, zaktualizowano komunikat błędu w przypadku braku samochodu o podanym identyfikatorze.

--- carsharing.py
+++ carsharing.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
+from schemas import load_db
 
 app = FastAPI()
-db = [
-    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
-    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
-    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
-    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
-    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
-]
+db = load_db()
 
 @app.get("/")
 async def welcome(name):
@@ -21,22 +12,22 @@ async def welcome(name):
 
 
 @app.get("/api/cars")
-def get_cars(size: str|None = None, doors: int|None = None) -> list[dict]:
+def get_cars(size: str|None = None, doors: int|None = None):
     result = db
     if size:
-        result = [car for car in result if car["size"] == size]
+        result = [car for car in result if car.size == size]
     if doors:
-        result = [car for car in result if car["doors"] >= doors]
+        result = [car for car in result if car.doors >= doors]
     return result
 
 
 @app.get("/api/cars/{id}")
-def car_by_id(id: int) -> dict:
-    result = [car for car in db if car["id"] == id]
+def car_by_id(id: int):
+    result = [car for car in db if car.id == id]
     if result:
         return result[0]
     else:
-        raise HTTPException(status_code=404, detail="Car not found")
+        raise HTTPException(status_code=404, detail=f"Car with id {id} not found")
 
 
 if __name__ == "__main__":
