82e3ae00d1e1c4b29c98ad817898be1f0ff013fd
Rafał Maculewicz <rafal.maculewicz@hotmail.com>
2025-07-04 13:54:39 +0200

    Dodano bazę danych samochodów oraz nowy endpoint API do pobierania dostępnych samochodów.

--- carsharing.py
+++ carsharing.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
 
 @app.get("/")
 async def welcome(name):
     """Return a friendly welcome message"""
     return {"message":f"Welcome, {name} to the Car Sharing API"}
+
+@app.get("/api/cars")
+def get_cars():
+    return db
